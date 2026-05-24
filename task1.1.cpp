7bdaba22c9f28de7772d2d5137f3dbc90822addf
vitaliilevitskyi7 <levitskiij@gmail.com>
2026-05-25 02:07:25 +0300

    Create task1.1.cpp

---
+++ task1.1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "2+31 = " << 2 + 31 << std::endl;
+    std::cout << "45*54-11 = " << 45 * 54 - 11 << std::endl;
+    std::cout << "15/4 = " << 15 / 4 << std::endl; // Цілочисельне ділення
+    std::cout << "15.0/4 = " << 15.0 / 4 << std::endl; // Дійсне ділення
+    std::cout << "67%5 = " << 67 % 5 << std::endl;
+    std::cout << "(2*45.1 +3.2)/2 = " << (2 * 45.1 + 3.2) / 2 << std::endl;
+    return 0;
+}
